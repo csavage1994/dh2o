a7d45ae2707b9dff955be6e58567d5eafb6bdcc2
csavage1994 <csavage1994@gmail.com>
2016-12-14 23:28:47 -0600

    Initial commit w/ raw header

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <siteHeader></siteHeader>
+  </div>
+</template>
+
+<script>
+import SiteHeader from './components/SiteHeader';
+
+export default {
+  name: 'app',
+  components: {
+    SiteHeader
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Roboto', sans-serif;
+  color: #2c3e50;
+}
+body, h1, div, span {
+  margin: 0;
+}
+h1 {
+  font-size: 1.5em;
+}
+</style>
---
+++ src/components/SiteHeader.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="header">
+    <h1 class="title">Chicago Crime Reports</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'siteHeader',
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.header {
+  width: 100vw;
+  height: 60px;
+  background: blue;
+}
+.title {
+  line-height: 60px;
+  color: white;
+  margin-left: 3vw;
+  display: inline-block;
+}
+</style>
